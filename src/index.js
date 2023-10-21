8588b2231d34df56cb834771d22748415fb497b8
gomess99 <gomess99@outlook.com>
2023-10-21 09:04:05 -0300

    Validação de usuário

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import connetcDatabase from "./database/db.js"
-import userRoute from "./routes/user.route.js";
 import dotenv from "dotenv";
+
+import userRoute from "./routes/user.route.js"
+import authRoute from "./routes/auth.route.js"
+
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -10,6 +14,7 @@ const app = express();
 connetcDatabase()
 app.use(express.json());
 app.use("/user", userRoute);
+app.use("/auth", authRoute);
 
 //ROTA
   //Method HTTP
@@ -23,4 +28,4 @@ app.use("/user", userRoute);
 
   //Function (Callback) - Responsável por executar algum comando
 
-  app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
